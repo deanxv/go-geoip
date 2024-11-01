e883e2884b090ac9e6720b446060ebb53c896752
dean <1006393151@qq.com>
2024-11-01 17:04:48 +0800

    init

--- go.mod
+++ go.mod
@@ -1,10 +1,15 @@
-module ip
+module ip2region-geoip
 
 go 1.23.2
 
 require (
+	github.com/gin-contrib/cors v1.7.2
+	github.com/gin-contrib/sessions v1.0.1
 	github.com/gin-gonic/gin v1.10.0
+	github.com/google/uuid v1.6.0
 	github.com/oschwald/maxminddb-golang v1.13.1
+	github.com/samber/lo v1.47.0
+	github.com/sony/sonyflake v1.2.0
 )
 
 require (
@@ -18,8 +23,12 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/gorilla/context v1.1.2 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
+	github.com/gorilla/sessions v1.2.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
@@ -31,7 +40,7 @@ require (
 	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
-	golang.org/x/text v0.15.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,240 +2,80 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 	"github.com/oschwald/maxminddb-golang"
 	"io"
+	"ip2region-geoip/common"
+	"ip2region-geoip/common/config"
+	logger "ip2region-geoip/common/loggger"
+	"ip2region-geoip/middleware"
+	"ip2region-geoip/router"
 	"log"
-	"net"
 	"net/http"
 	"os"
-	"strings"
-	"sync"
+	"strconv"
 	"time"
 )
 
-// ASN represents the ASN database structure.
-type ASN struct {
-	Number       uint   `maxminddb:"autonomous_system_number"`
-	Organization string `maxminddb:"autonomous_system_organization"`
-}
-
-// City represents the City database structure.
-type City struct {
-	Country           CountryInfo   `maxminddb:"country"`
-	RegisteredCountry CountryInfo   `maxminddb:"registered_country"`
-	Subdivisions      []Subdivision `maxminddb:"subdivisions"`
-	City              NameInfo      `maxminddb:"city"`
-}
-
-// CountryInfo represents country information in the database.
-type CountryInfo struct {
-	ISOCode string            `maxminddb:"iso_code"`
-	Names   map[string]string `maxminddb:"names"`
-}
-
-// Subdivision represents subdivision information in the database.
-type Subdivision struct {
-	Names map[string]string `maxminddb:"names"`
-}
-
-// NameInfo represents name information in the database.
-type NameInfo struct {
-	Names map[string]string `maxminddb:"names"`
-}
-
-// GeoCN represents the GeoCN database structure.
-type GeoCN struct {
-	Province  string `maxminddb:"province"`
-	City      string `maxminddb:"city"`
-	Districts string `maxminddb:"districts"`
-	ISP       string `maxminddb:"isp"`
-	Net       string `maxminddb:"net"`
-}
-
-var (
-	cityReader, asnReader, cnReader *maxminddb.Reader
-	asnMap                          = map[uint]string{
-		9812: "东方有线", 9389: "中国长城", 17962: "天威视讯",
-		// Add more mappings as needed
-	}
-	mu sync.Mutex
-)
-
-func getAsInfo(number uint) string {
-	return asnMap[number]
-}
-
-func getDescription(names map[string]string) string {
-	if name, ok := names["zh-CN"]; ok {
-		return name
-	}
-	return names["en"]
-}
-
-func getCountry(d map[string]string) string {
-	r := getDescription(d)
-	switch r {
-	case "香港", "澳门", "台湾":
-		return "中国" + r
-	default:
-		return r
-	}
-}
-
-func deDuplicate(regions []string) []string {
-	unique := make(map[string]bool)
-	var result []string
-	for _, region := range regions {
-		if _, ok := unique[region]; !ok && region != "" {
-			unique[region] = true
-			result = append(result, region)
-		}
-	}
-	return result
-}
-
-func getMaxmind(ip string) (map[string]interface{}, error) {
-	ret := make(map[string]interface{})
-	ret["ip"] = ip
-
-	parsedIP := net.ParseIP(ip)
-	if parsedIP == nil {
-		return nil, fmt.Errorf("invalid IP address")
-	}
-
-	var asn ASN
-	mu.Lock()
-	defer mu.Unlock()
-	if err := asnReader.Lookup(parsedIP, &asn); err == nil {
-		asInfo := map[string]interface{}{
-			"number": asn.Number,
-			"name":   asn.Organization,
-		}
-		if info := getAsInfo(asn.Number); info != "" {
-			asInfo["info"] = info
-		}
-		ret["as"] = asInfo
-	}
-
-	var city City
-	if network, ok, err := cityReader.LookupNetwork(parsedIP, &city); err == nil && ok {
-		ret["addr"] = network.String()
-		if city.Country.ISOCode != "" {
-			ret["country"] = map[string]string{
-				"code": city.Country.ISOCode,
-				"name": getCountry(city.Country.Names),
-			}
-		}
-		if city.RegisteredCountry.ISOCode != "" {
-			ret["registered_country"] = map[string]string{
-				"code": city.RegisteredCountry.ISOCode,
-				"name": getCountry(city.RegisteredCountry.Names),
-			}
-		}
-		var regions []string
-		for _, subdivision := range city.Subdivisions {
-			regions = append(regions, getDescription(subdivision.Names))
-		}
-		if cityName := getDescription(city.City.Names); cityName != "" {
-			regions = append(regions, cityName)
-		}
-		ret["regions"] = deDuplicate(regions)
-	}
-
-	return ret, nil
-}
-
-func getCn(ip string, info map[string]interface{}) {
-	parsedIP := net.ParseIP(ip)
-	if parsedIP == nil {
-		return
-	}
-
-	var geoCN GeoCN
-	mu.Lock()
-	defer mu.Unlock()
-	if network, ok, err := cnReader.LookupNetwork(parsedIP, &geoCN); err == nil && ok {
-		info["addr"] = network.String()
-		regions := deDuplicate([]string{geoCN.Province, geoCN.City, geoCN.Districts})
-		if len(regions) > 0 {
-			info["regions"] = regions
-		}
-		if _, ok := info["as"]; !ok {
-			info["as"] = make(map[string]interface{})
-		}
-		info["as"].(map[string]interface{})["info"] = geoCN.ISP
-		if geoCN.Net != "" {
-			info["type"] = geoCN.Net
-		}
-	}
-}
-
-func getIpInfo(ip string) map[string]interface{} {
-	info, err := getMaxmind(ip)
-	if err != nil {
-		return map[string]interface{}{"error": err.Error()}
-	}
-	if country, ok := info["country"].(map[string]string); ok && country["code"] == "CN" {
-		if regCountry, ok := info["registered_country"].(map[string]string); !ok || regCountry["code"] == "CN" {
-			getCn(ip, info)
-		}
-	}
-	return info
-}
-
 func main() {
-	r := gin.Default()
+	logger.SetupLogger()
+	logger.SysLog(fmt.Sprintf("ip2region-geoip %s started", common.Version))
 
-	// 处理没有参数的情况，使用请求方的 IP
-	r.GET("/ip", func(c *gin.Context) {
-		ip := getRealClientIP(c)
-		info := getIpInfo(ip)
-		c.JSON(http.StatusOK, info)
-	})
+	if os.Getenv("GIN_MODE") != "debug" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+	if config.DebugEnabled {
+		logger.SysLog("running in debug mode")
+	}
+	var err error
 
-	r.GET("/ip/:ip", func(c *gin.Context) {
-		ip := c.Param("ip")
-		if ip == "" {
-			ip = c.ClientIP()
-		}
-		info := getIpInfo(ip)
-		c.JSON(http.StatusOK, info)
-	})
+	server := gin.New()
+	server.Use(gin.Recovery())
+	server.Use(middleware.RequestId())
+	middleware.SetUpLogger(server)
+	store := cookie.NewStore([]byte(config.SessionSecret))
+	server.Use(sessions.Sessions("session", store))
+
+	router.SetRouter(server)
+	var port = os.Getenv("PORT")
+	if port == "" {
+		port = strconv.Itoa(*common.Port)
+	}
 
 	go scheduleDatabaseUpdate()
 
-	if err := r.Run(":7099"); err != nil {
-		log.Fatal(err)
+	err = server.Run(":" + port)
+	if err != nil {
+		logger.FatalLog("failed to start HTTP server: " + err.Error())
 	}
 }
 
-// getRealClientIP 尝试从多个头部信息获取真实 IP，如果没有则使用 c.ClientIP()
-func getRealClientIP(c *gin.Context) string {
-	// 尝试从 X-Forwarded-For 头获取
-	if xff := c.GetHeader("X-Forwarded-For"); xff != "" {
-		// 分割 X-Forwarded-For 字符串，获取第一个 IP 地址
-		ips := strings.Split(xff, ",")
-		if len(ips) > 0 {
-			realIP := strings.TrimSpace(ips[0]) // 清除可能的空格
-			log.Printf("X-Forwarded-For IP: %s", realIP)
-			return realIP
-		}
-	}
-	// 尝试从 X-Real-IP 头获取
-	if xrip := c.GetHeader("X-Real-IP"); xrip != "" {
-		log.Printf("X-Real-IP: %s", xrip)
-		return xrip
-	}
-	// 默认使用 Gin 的 ClientIP 方法
-	clientIP := c.ClientIP()
-	log.Printf("Default ClientIP: %s", clientIP)
-	return clientIP
-}
-
-func init() {
-	loadDatabases()
-}
+//func main() {
+//	r := gin.Default()
+//
+//	// 处理没有参数的情况，使用请求方的 IP
+//	r.GET("/ip", func(c *gin.Context) {
+//		ip := getRealClientIP(c)
+//		info := getIpInfo(ip)
+//		c.JSON(http.StatusOK, info)
+//	})
+//
+//	r.GET("/ip/:ip", func(c *gin.Context) {
+//		ip := c.Param("ip")
+//		if ip == "" {
+//			ip = c.ClientIP()
+//		}
+//		info := getIpInfo(ip)
+//		c.JSON(http.StatusOK, info)
+//	})
+//
+//
+//	if err := r.Run(":7099"); err != nil {
+//		log.Fatal(err)
+//	}
+//}
 
 func loadDatabases() {
 	// 检查文件是否存在，如果不存在则下载
@@ -244,17 +84,17 @@ func loadDatabases() {
 	downloadAndSave("GeoCN.mmdb", "http://github.com/ljxi/GeoCN/releases/download/Latest/GeoCN.mmdb")
 
 	var err error
-	mu.Lock()
-	defer mu.Unlock()
-	cityReader, err = maxminddb.Open("GeoLite2-City.mmdb")
+	common.Mu.Lock()
+	defer common.Mu.Unlock()
+	common.CityReader, err = maxminddb.Open("GeoLite2-City.mmdb")
 	if err != nil {
 		log.Fatalf("Error opening city database: %v", err)
 	}
-	asnReader, err = maxminddb.Open("GeoLite2-ASN.mmdb")
+	common.AsnReader, err = maxminddb.Open("GeoLite2-ASN.mmdb")
 	if err != nil {
 		log.Fatalf("Error opening ASN database: %v", err)
 	}
-	cnReader, err = maxminddb.Open("GeoCN.mmdb")
+	common.CnReader, err = maxminddb.Open("GeoCN.mmdb")
 	if err != nil {
 		log.Fatalf("Error opening CN database: %v", err)
 	}
@@ -282,6 +122,8 @@ func downloadAndSave(filename, url string) {
 }
 
 func scheduleDatabaseUpdate() {
+	loadDatabases()
+
 	ticker := time.NewTicker(3 * time.Minute)
 	defer ticker.Stop()
 
